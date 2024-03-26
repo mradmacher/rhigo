3fc36e3acb5810866278b83b9ec045b6477b5e6e
mradmacher <michal@radmacher.pl>
2024-03-26 13:57:27 +0100

    make rigol and rohde&schwarz talk

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
+# https://www.rohde-schwarz.com/webhelp/smb100a_html_usermanual_1/Content/2e097e6397fa40bd.htm#d79403e12303
 # https://github.com/pyvisa/pyvisa-py
 # pip install pyvisa-py
 # pip install pyusb
 # pip install psutil
 # pip install zeroconf
-
 import pyvisa
-
+import time
+import re
 rm = pyvisa.ResourceManager('@py')
 
-print(rm.list_resources())
-# ('USB0::0x1AB1::0x0588::DS1K00005888::INSTR')
-# inst = rm.open_resource('USB0::0x1AB1::0x0588::DS1K00005888::INSTR')
-# print(inst.query("*IDN?"))
+steps = [
+  ('300000', '8dBm'),
+  ('350000', '9dBm'),
+  ('400000', '10dBm'),
+]
 
+print(rm.list_resources('?*'))
+rohde_schwarz_name = 'TCPIP::192.168.1.5::INSTR'
+rigol_name = 'TCPIP::192.168.1.11::INSTR'
+rohde_schwarz = rm.open_resource(rohde_schwarz_name)
+# instr.read_termination = '\n'
+# instr.write_termination = '\n'
+# instr.baud_rate = 9600
+idn_result = rohde_schwarz.query('*idn?')
+print(idn_result)
+assert re.match('Rohde&Schwarz', idn_result)
 
+rigol = rm.open_resource(rigol_name)
+idn_result = rigol.query('*idn?')
+print(idn_result)
+assert re.match('Rigol', idn_result)
 
-# python-vxi11
-# import vxi11
-# import numpy as np
-# inst = vxi11.Instrument('TCPIP0::192.168.0.254::INSTR')
-# print(inst.ask('*IDN?'))
-# inst.write('SYST:LANG "SCPI"') 
+rohde_schwarz.write('*rst; status:preset; *cls')
+#rigol.write('status:preset')
+#print(rigol.write('*rst; status:preset; *cls'))
+time.sleep(5)
+
+rohde_schwarz.write('syst:disp:upd 1')
+rigol.write('cal:mark:peak:sear:mode max')
+
+print('Start')
+for step in steps:
+    rohde_schwarz.write('freq:cw {0}Hz'.format(step[0]))
+    rohde_schwarz.write('sour:pow:lev:imm:ampl {0}'.format(step[1]))
+    rigol.write('sens:freq:cent {0}'.format(step[0]))
+    rohde_schwarz.write('outp 1')
+    time.sleep(5)
+    print(rigol.query('fetc:harm:amp:all?'))
+    rohde_schwarz.write('outp 0')
+rohde_schwarz.write('*rst; status:preset; *cls')
